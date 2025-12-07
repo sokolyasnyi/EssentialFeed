e7a309d8e102df6c181ec7cd408746e40254fc6b
Stanislav Sokolov <stasik_sokolov1899@mail.ru>
2025-12-07 15:36:03 +0300

    Create FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Станислав Соколов on 12/7/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
