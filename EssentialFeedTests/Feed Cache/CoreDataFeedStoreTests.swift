ab4a70bc493a158616cbd6361ea837de2ef275c1
Stanislav Sokolov <stasik_sokolov1899@mail.ru>
2026-01-29 21:30:57 +0300

    `CoreDataFeedStore.retrieve()` has no side-effects on empty cache

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -14,9 +14,11 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
 
         assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
-    
-    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
 
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
+
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
@@ -60,7 +62,7 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
 
     // MARK: Helpers
-    
+
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
         let sut = CoreDataFeedStore()
         trackForMemoryLeaks(sut, file: file, line: line)
