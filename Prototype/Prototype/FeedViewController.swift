fe9452c336d7b5e79ade1c6191b3873f0c426552
Stanislav Sokolov <stasik_sokolov1899@mail.ru>
2026-02-05 21:28:18 +0300

    Add `FeedViewController` rendering 10 cells

---
+++ Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  Prototype
+//
+//  Created by Станислав Соколов on 2/5/26.
+//
+
+import UIKit
+
+final class FeedViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+    }
+}
