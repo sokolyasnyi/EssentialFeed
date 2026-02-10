2fd77e691fc95dcb3d80b1e8db7f38cd30a2b051
Stanislav Sokolov <stasik_sokolov1899@mail.ru>
2026-02-10 23:14:25 +0300

    Capture `self` instead `view` to predict deallocate view

--- EssentialFeediOS/Feed UI/Controllers/FeedRefreshController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshController.swift
@@ -21,11 +21,11 @@ public final class FeedRefreshController: NSObject {
     }
 
     private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
-        viewModel.onLoadingStateChange = { [weak view] isLoading in
+        viewModel.onLoadingStateChange = { [weak self] isLoading in
             if isLoading {
-                view?.beginRefreshing()
+                self?.view.beginRefreshing()
             } else {
-                view?.endRefreshing()
+                self?.view.endRefreshing()
             }
         }
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
