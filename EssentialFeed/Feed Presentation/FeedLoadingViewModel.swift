16122d36cea7f4c215dc15cc3e0ddb140303d147
Stanislav Sokolov <stasik_sokolov1899@mail.ru>
2026-03-02 22:55:42 +0300

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Станислав Соколов on 3/2/26.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
