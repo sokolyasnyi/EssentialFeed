8e6936d2671dc0fd1d8ad6a743bcec57b923214e
Stanislav Sokolov <stasik_sokolov1899@mail.ru>
2026-03-16 21:48:09 +0300

    Move Feed UI composition details from EssentialFeediOS to the EssentialApp module (Composition Root)

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -5,8 +5,9 @@
 //  Created by Станислав Соколов on 2/15/26.
 //
 
-import EssentialFeed
 import UIKit
+import EssentialFeed
+import EssentialFeediOS
 
 final class FeedViewAdapter: FeedView {
 
@@ -19,7 +20,7 @@ final class FeedViewAdapter: FeedView {
     }
 
     func display(_ viewModel: FeedViewModel) {
-        controller?.tableModel = viewModel.feed.map { model in
+        controller?.display(viewModel.feed.map { model in
             let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
             let view = FeedImageCellController(delegate: adapter)
 
@@ -27,6 +28,6 @@ final class FeedViewAdapter: FeedView {
                 view: WeakRefVirtualProxy(view),
                 imageTransformer: UIImage.init)
             return view
-        }
+        })
     }
 }
