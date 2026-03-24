0308970aba2d02eef664b039a669063b8e7de2bf
Stanislav Sokolov <stasik_sokolov1899@mail.ru>
2026-03-24 21:37:07 +0300

    Replace FeedImageDataLoader composition with Combine operators

--- EssentialApp/EssentialApp/FeedImageDataLoaderPresentationAdapter.swift
+++ EssentialApp/EssentialApp/FeedImageDataLoaderPresentationAdapter.swift
@@ -5,17 +5,19 @@
 //  Created by Станислав Соколов on 2/15/26.
 //
 
+import Combine
+import Foundation
 import EssentialFeed
 import EssentialFeediOS
 
 final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: FeedImageCellControllerDelegate where View.Image == Image {
     private let model: FeedImage
-    private let imageLoader: FeedImageDataLoader
-    private var task: FeedImageDataLoaderTask?
+    private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
+    private var cancellable: Cancellable?
 
     var presenter: FeedImagePresenter<View, Image>?
 
-    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+    init(model: FeedImage, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
         self.model = model
         self.imageLoader = imageLoader
     }
@@ -23,18 +25,24 @@ final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image>: 
     func didRequestImage() {
         presenter?.didStartLoadingImageData(for: model)
 
+
+
         let model = self.model
-        task = imageLoader.loadImageData(from: model.imageURL) { [weak self] result in
-            switch result {
-            case let .success(data):
-                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingImageData(with: error, for: model)
-            }
-        }
+
+        cancellable = imageLoader(model.imageURL).sink(
+            receiveCompletion: { [weak self] completion in
+                switch completion {
+                case .finished: break
+
+                case let .failure(error):
+                    self?.presenter?.didFinishLoadingImageData(with: error, for: model)
+                }
+        }, receiveValue: { [weak self] data  in
+            self?.presenter?.didFinishLoadingImageData(with: data, for: model)
+        })
     }
 
     func didCancelImageRequest() {
-        task?.cancel()
+        cancellable?.cancel()
     }
 }
--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -43,17 +43,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func configureWindow() {
-        let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
-
-        let localImageLoader = LocalFeedImageDataLoader(store: store)
-
         window?.rootViewController = UINavigationController(rootViewController: FeedUIComposer.feedComposedWith(
             feedLoader: makeRemoteFeedLoaderWithLocalFallback,
-            imageLoader: FeedImageDataLoaderWithFallbackComposite(
-                primary: FeedImageDataLoaderCacheDecorator(
-                    decoratee: remoteImageLoader,
-                    cache: localImageLoader),
-                fallback: localImageLoader)))
+            imageLoader: makeLocalImageLoaderWithRemoteFallback))
         
         window?.makeKeyAndVisible()
     }
@@ -71,6 +63,49 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .loadPublisher()
             .caching(to: localFeedLoader)
             .fallback(to: localFeedLoader.loadPublisher)
+    }
+
+    private func makeLocalImageLoaderWithRemoteFallback(url: URL) -> FeedImageDataLoader.Publisher {
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
+        let localImageLoader = LocalFeedImageDataLoader(store: store)
+
+        return localImageLoader
+            .loadImageDataPublisher(for: url)
+            .fallback(to: {
+                remoteImageLoader
+                    .loadImageDataPublisher(for: url)
+                    .caching(to: localImageLoader, using: url)
+            })
+    }
+}
+
+public extension FeedImageDataLoader {
+    typealias Publisher = AnyPublisher<Data, Error>
+
+    func loadImageDataPublisher(for url: URL) -> Publisher {
+        var task: FeedImageDataLoaderTask?
+
+        return Deferred {
+            Future { completion in
+                task = self.loadImageData(from: url, completion: completion)
+            }
+        }
+        .handleEvents(receiveCancel: { task?.cancel() })
+        .eraseToAnyPublisher()
+    }
+}
+
+extension Publisher where Output == Data {
+    func caching(to cache: FeedImageDataCache, using url: URL) -> AnyPublisher<Output, Failure> {
+        handleEvents(receiveOutput: { data in
+            cache.saveIgnoringResult(data, for: url)
+        }).eraseToAnyPublisher()
+    }
+}
+
+private extension FeedImageDataCache {
+    func saveIgnoringResult(_ data: Data, for url: URL) {
+        save(data, for: url) { _ in }
     }
 }
 
