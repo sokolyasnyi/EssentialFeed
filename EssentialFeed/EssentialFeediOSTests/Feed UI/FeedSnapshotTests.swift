d1c2163618d2edcb81449cbfcb4b4ceda1b71cdb
Stanislav Sokolov <stasik_sokolov1899@mail.ru>
2026-05-19 19:38:27 +0300

    Fix ListViewController snapshot tests

--- EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -121,7 +121,7 @@ private extension ListViewController {
         let cells: [CellController] = stubs.map { stub in
             let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, selection: {})
             stub.controller = cellController
-            return CellController(cellController)
+            return CellController(id: UUID(), cellController)
         }
 
         display(cells)
